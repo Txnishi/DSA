8af318e860062e7f3b47414668882858c6641f1c
Tanishi Devlal <104810843+Txnishi@users.noreply.github.com>
2022-05-08 08:33:57 +0530

    Create pointer.cpp

---
+++ Assignment 3/pointer.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+void update(int *a,int *b) {
+    // Complete this function   
+     int c = *b;
+     if(*a<*b){
+         *b= (*b)-(*a);
+        }else{
+            *b = (*a) - (*b);
+        }
+    *a = (*a)+c;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
