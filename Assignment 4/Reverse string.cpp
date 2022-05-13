ab8bcfd667e5473cf0789b7a57012edb501d247b
Tanishi Devlal <104810843+Txnishi@users.noreply.github.com>
2022-05-13 11:02:50 +0530

    Create Reverse string.cpp

---
+++ Assignment 4/Reverse string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int len= s.size();
+        int j = len - 1;
+        for(int i=0; i< len/2; i++){
+            swap(s[i], s[j]);
+            j--;
+        }
+        for(int i=0; i< len; i++){
+            cout<<s[i]<<" ";
+        }
+    }
+};
