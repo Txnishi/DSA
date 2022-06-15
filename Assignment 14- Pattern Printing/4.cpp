0d1adffa2e060d710cffd2ebbcd22ace06544b6c
Tanishi Devlal <104810843+Txnishi@users.noreply.github.com>
2022-06-15 13:32:29 +0530

    Create 4.cpp

---
+++ Assignment 14- Pattern Printing/4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+
+int num, len, temp;
+cin>>num;
+
+
+while(num!= 0){
+for(int i=0; i<num; i++){
+	cout<<"#";
+}
+cout<<endl;
+num = num/2;
+}
+	return 0;
+}
