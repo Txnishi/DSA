69a89255a5a66886e0835c80e7a1746a2c831e0a
Tanishi Devlal <104810843+Txnishi@users.noreply.github.com>
2022-07-15 10:43:37 +0530

    Create tree-height-of-a-binary-tree.cpp

---
+++ Assignment 26 (trees)/tree-height-of-a-binary-tree.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*The tree node has data, left child and right child 
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+    int height(Node* root) {
+        // Write your code here.
+        
+        queue<Node*>Q;
+        Q.push(root);
+        
+        int level=0;
+        
+        if(!root) return 0;
+        while(!Q.empty()){
+            int size = Q.size();
+            while(size--){
+                Node* temp = Q.front();
+                
+                if(temp->left){
+                    Q.push(temp->left);
+                }
+                if(temp->right){
+                    Q.push(temp->right);
+                }             
+                Q.pop();
+            }
+            level++;
+        }
+        return level-1;
+    }
+
+}; //End of Solution
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+    int height = myTree.height(root);
+    
+  	std::cout << height;
+
+    return 0;
+}
