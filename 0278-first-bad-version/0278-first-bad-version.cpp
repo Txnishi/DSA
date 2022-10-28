410bd5419f11ca62d73466deec4050a9e172bbad
Tanishi Devlal <104810843+Txnishi@users.noreply.github.com>
2022-10-28 10:03:09 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (88.69%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -7,35 +7,35 @@ public:
         
         long long int i = 0;
         long long int len = n-1;
-        vector<int>ans;
         
-        if(isBadVersion(1)){
-            return 1;
-        }
+        // vector<int>ans;
         
-        if(isBadVersion(n) && !isBadVersion(n-1)){
-            return n;
-        }
+//         if(isBadVersion(1)){
+//             return 1;
+//         }
+        
+//         if(isBadVersion(n) && !isBadVersion(n-1)){
+//             return n;
+        // }
         
         while(i<=len){
             long long int mid = (i+len)/2;
             
             if(isBadVersion(mid)){
                 len = mid-1;
-                ans.push_back(mid);
+                // ans.push_back(mid);
             }
             
             if(!isBadVersion(mid)){
                 i = mid+1;
             }
             
-            
         }
         
-        sort(ans.begin(), ans.end());
+        // sort(ans.begin(), ans.end());
         
         
         
-        return ans[0];
+        return i;
     }
 };
