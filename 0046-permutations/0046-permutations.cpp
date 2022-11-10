ca877124145b90b1270a2245fba0a0ff48e645a8
Tanishi Devlal <104810843+Txnishi@users.noreply.github.com>
2022-11-10 17:05:57 +0530

    Time: 10 ms (23.59%), Space: 8.1 MB (28.99%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,36 @@
+class Solution {
+private:
+    void recur(vector<int>A , int i, vector<vector<int>> &res){
+        
+        if(i >= A.size()){
+            res.push_back(A);
+        } 
+        
+        int temp;
+        
+        for(int j = i; j< A.size(); j++){
+            
+            temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+            
+            recur(A, i+1, res);
+            
+            temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+            
+        } 
+    }
+    
+    
+public:
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        int a =0;
+        
+        recur(nums,a,ans);
+        return ans;
+    }
+};
