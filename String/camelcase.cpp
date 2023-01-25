48186ec2a5d1252d93f8e21a1350752e8eb079e6
Tanishi Devlal <104810843+Txnishi@users.noreply.github.com>
2023-01-25 11:06:56 +0530

    Create camelcase.cpp

---
+++ String/camelcase.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'camelcase' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int camelcase(string s) {
+    
+    int len = s.length();
+    int count =1;
+    for(int i =0; i< len; i++){
+        if(s[i] < 97){
+            count++;
+        }
+    }
+    
+    return count;
+    
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    int result = camelcase(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
